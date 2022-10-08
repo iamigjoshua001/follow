173a21c0f3c39ce81ca50fc8b88eda86c4f950b6
iamjoshua001 <joshuaosarodion5@gmailcom>
2022-10-09 00:21:44 +0100

    hello

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
+const routesAdmin = require("./routes/routesAdmin");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyparser.json());
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("data base login");
+});
+
+console.log(Boolean(NaN));
+
+app.get("/", (req, res) => {
+  res.send("welcome to my server");
+});
+
+app.use("/admin", routesAdmin);
+app.use("/user", userRoutes);
+
+app.listen(7000, () => console.log("server is runing"));
